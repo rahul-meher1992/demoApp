c59a6337550903688e3a40c36c57d7d9ae62a1b5
Rahul <rahul.m@neutrinos.co>
2024-02-10 00:48:05 +0530

    Added show hide functionality

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input  } from '@angular/core';
 import {AppServicesService} from '../../services/app-services.service';
 
 @Component({
@@ -8,17 +8,31 @@ import {AppServicesService} from '../../services/app-services.service';
 })
 export class TimelineComponent implements OnInit {
 
-  posts:any =[]
+  posts:any =[];
 
-  constructor(private appServicesService:AppServicesService) { }
+  constructor(private appServicesService:AppServicesService) {}
 
   ngOnInit(): void {
      //call the method from service file using HTTP Client
      this.appServicesService.getPostData().subscribe((data= [])=>{
       this.posts = data
+      this.posts.forEach((obj:any) => {
+        obj.showButtonVisible = true;
+        obj.hideButtonVisible = false;
+      });
       console.log(this.posts)
      })
-    
   }
 
+  toggleButtons(index: number): void {
+    const post = this.posts[index];
+    // console.log(post)
+    if(post.showButtonVisible){
+      post.showButtonVisible = false;
+      post.hideButtonVisible = true
+    }else{
+      post.showButtonVisible = true;
+      post.hideButtonVisible = false
+    }
+  }
 }
