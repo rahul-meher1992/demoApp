cb19460c41465e4da68c602304a2725cfbc4dd9a
Rahul <rahul.m@neutrinos.co>
2024-02-15 01:36:12 +0530

    Added set timeout to resolve the issue

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,31 +1,21 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent implements OnChanges {
+export class CommentsComponent implements OnInit {
   @Input() comments: any[] = [];
   @Input() postId!: number;
   particularCommentData: any;
 
   constructor() { }
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.comments || changes.postId) {
-      this.filterComments();
-    }
-  }
-
-
-
-  /**
-  * 
-  * @returns filtered data based on post id
-  */
-
-  filterComments() {
-    this.particularCommentData = this.comments.filter(comment => comment.postId === this.postId);
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.particularCommentData = this.comments.filter(comment => comment.postId === this.postId);
+    }, 1000);
+      
   }
 
 }
